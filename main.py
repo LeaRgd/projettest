c9c2c27684165af5dc09a4e220a61f11078e6295
LeaRgd <lea.rigaud@laposte.net>
2022-03-30 15:06:49 +0200

    add couleur

---
+++ main.py
@@ -0,0 +1,17 @@
+
+class classe():
+    def_init_(self):
+        self.name="lea"
+        self.couleur="bleu"
+        
+
+
+def function(a,b):
+    return a+b
+
+print ("Hello")
+
+
+abc=2
+
+li=[1,2,3]
